a135ef196611ba48e13062af2ae240641781d152
BalloIbrahima <ibrahimaballo01@gmail.com>
2023-01-19 14:41:06 +0000

    edit compte ok

--- src/app/compte/compte.page.ts
+++ src/app/compte/compte.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-compte',
@@ -7,9 +8,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ComptePage implements OnInit {
 
-  constructor() { }
+  //user infos
+  nomComplet:any;
+  username:any;
+  email:any;
+  dateNaissance:any;
+
+  ///
+  message:any
+  erreur:any
+  fichier:any
+  imgChosed:any='assets/images/moi.jpg'
+
+  constructor(private router:Router) { }
 
   ngOnInit() {
+
+  }
+
+
+  //////
+  clickFile(){
+   var input=<HTMLIonCardElement>document.querySelector('.fileselect')
+    input.click();
+  }
+  changeFile(e:any){
+    //verification si une photo a été choisie ou pas
+    if(!e.target.files[0] || e.target.files[0].length==0){
+      this.message="Vous devez choisir une image !";
+      this.erreur=true;
+      return;
+    }
+
+    //verification du type de fichier choisi pour recaler si ce n'est pas une photo
+    var typeFichier=e.target.files[0].type;
+    //this.imgChosed=e.target['files'];
+
+
+    if(e.target.files){
+      var reader= new FileReader();
+      reader.readAsDataURL(e.target.files[0]);
+      reader.onload=(event:any)=>{
+        this.message="";
+        this.imgChosed=event.target.result;
+        this.fichier=e.target['files'][0];
+      }
+    }
+  }
+
+  ///creation de compte
+  singup($event:any){
+
+
+    this.router.navigate(['/tabs'])
+
   }
 
 }
