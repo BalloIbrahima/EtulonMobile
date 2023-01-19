ab55b9b9d277636023c3cff8fc9927c7e1f89e83
BalloIbrahima <ibrahimaballo01@gmail.com>
2023-01-19 10:11:00 +0000

    play level detail ok

--- src/app/game1/game1.page.ts
+++ src/app/game1/game1.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-game1',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Game1Page implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit() {
   }
 
+
+  //
+  goToNextPage(){
+    //console.log('gooooo')
+    this.router.navigate(['/game2'])
+  }
 }
--- src/app/game2/game2.page.ts
+++ src/app/game2/game2.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-game2',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Game2Page implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit() {
+  }
+
+   //
+   Play(){
+    //console.log('gooooo')
+    this.router.navigate(['/game2'])
   }
 
 }
