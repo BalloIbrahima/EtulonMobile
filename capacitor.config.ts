2bfc25dda5abb8d5d7483d500262841f35ba9289
BalloIbrahima <ibrahimaballo01@gmail.com>
2023-01-20 10:10:35 +0000

    before push

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'AppGame',
+  appId: 'com.ib.etulon',
+  appName: 'E-Tulon',
   webDir: 'www',
   bundledWebRuntime: false
 };
