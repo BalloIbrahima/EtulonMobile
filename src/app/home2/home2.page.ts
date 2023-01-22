d4a5a35cedc7d089df5e0ebe7d448fd536cd5e7b
BalloIbrahima <ibrahimaballo01@gmail.com>
2023-01-22 18:20:47 +0000

    update home 2

--- src/app/home2/home2.page.ts
+++ src/app/home2/home2.page.ts
@@ -25,12 +25,24 @@ export class Home2Page implements OnInit {
   check(numero:any){
    var checkbox= <HTMLIonCheckboxElement>document.querySelector('#card'+numero)
    var card=<HTMLIonCardElement>document.querySelector('.card'+numero)
+   var i1= <HTMLIonCheckboxElement>document.querySelector('#iok'+numero)
+   var i2= <HTMLIonCheckboxElement>document.querySelector('#ino'+numero)
+   var img= <HTMLIonCheckboxElement>document.querySelector('#img'+numero)
 
    if(checkbox.checked==true){
-    card.style.border='4px solid white'
+    card.style.backgroundColor='#fff'
+    card.style.color='#000'
+    //i2.style.color='#fff'
+    i2.style.color='#fff'
+    i1.classList.remove('d-none')
+    i2.classList.add('d-none')
    }
    else{
-    card.style.border='4px solid green'
+    card.style.backgroundColor='#008000'
+    card.style.color='#fff'
+    i1.classList.add('d-none')
+    i2.classList.remove('d-none')
+    //card.style.border='4px solid green'
    }
    checkbox.checked=!checkbox.checked
   }
